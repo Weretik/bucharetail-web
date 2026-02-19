e4a0091478f1f5656a4a85a84946ae5a60b2d126
Vitaliy <VitaliyThupin@gmail.com>
2026-02-19 05:28:47 +0200

    fix(ui): remove unused components and update layout imports
    - Removed FAQ, Stats, Main Content, and Pricing components to streamline the UI
    - Updated app imports to include only relevant and active components
    - Simplified the overall structure for better maintainability

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,16 @@ import { Component, signal } from '@angular/core';
 import {Hero} from '../ui/hero/hero';
 import {Header} from '../ui/header/header';
 import {Footer} from '../ui/footer/footer';
-import {Stats} from '../ui/stats/stats';
-import {Faq} from '../ui/faq/faq';
 import {Features} from '../ui/features/features';
-import {MainContent} from '../ui/main-content/main-content';
-import {Pricing} from '../ui/pricing/pricing';
+import {Area} from '../ui/area/area';
+import {Advantages} from '../ui/advantages/advantages';
+import {Galleria} from '../ui/galleria/galleria';
+import {CallToActions} from '../ui/call-to-actions/call-to-actions';
+
 
 @Component({
   selector: 'app-root',
-  imports: [Hero, Header, Footer, Stats, Faq, Features, MainContent, Pricing],
+  imports: [Hero, Header, Footer, Features, Area, Advantages, Galleria, CallToActions],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
